9707b14aec2c4bba766b9e09d3920ecc8da208d0
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-03-20 22:14:31 +0000

    Fire laser implemented

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _multiplier = 8;
+
+    [SerializeField]
+    private float maxYVal = 8;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(MoveUntil());
+    }
+
+    IEnumerator MoveUntil()
+    {
+        while (transform.position.y < maxYVal)
+        {
+            transform.Translate(Vector3.up * _multiplier * Time.deltaTime);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
+
+
+
+
+
+// Lifespan model:
+//[SerializeField]
+//private float _lifeSpan = 6;
+
+//float elapsedTime = 0;
+//        while (elapsedTime<_lifeSpan)
+//        {
+//            transform.Translate(Vector3.up* _multiplier * Time.deltaTime);
+//elapsedTime += Time.deltaTime;
+//            yield return null;
+//        }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour // Player inherits or extends monobehaviour.
     // public or private. If private, only this class knows it exists.
     // data types. Key data types (int, float, bool, string)
 
+    [Header("Movement")]
     [SerializeField]
     private float _multiplier = 3.5f;
 
@@ -14,24 +15,26 @@ public class Player : MonoBehaviour // Player inherits or extends monobehaviour.
     private float _yMaxVal = 0;
     private float _yMinVal = -4;
 
-    //public float horizontalInput;
+    [Header("Shooting")]
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _nextFire = 0;
 
     void Start()
     {
-        // Take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
-    
+
     void Update()
     {
-        // Vector3.right is the equivalent of new Vector3(1,0,0).
-        // Which means moving us 1 Unity unit (a meter) per frame (so roughly 60 meters per second)
-        // Incorporate real time to get this to happen per second instead
-        //transform.Translate(Vector3.right * _multiplier * Time.deltaTime);
-
         CalculateMovement();
-
+        Shooting();
     }
+
+    // *******************************************************************************************
+    // MOVEMENT
 
     private void CalculateMovement()
     {
@@ -45,13 +48,13 @@ public class Player : MonoBehaviour // Player inherits or extends monobehaviour.
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * _multiplier * Time.deltaTime);          
-    } 
+        transform.Translate(direction * _multiplier * Time.deltaTime);
+    }
 
     private void Clamping()
     {
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _yMinVal, _yMaxVal), transform.position.z);
-        
+
         if (transform.position.x >= _xClamping)
         {
             transform.position = new Vector3(-11, transform.position.y, transform.position.z);
@@ -62,17 +65,47 @@ public class Player : MonoBehaviour // Player inherits or extends monobehaviour.
         }
     }
 
+    // *******************************************************************************************
+    // SHOOTING
+
+    private void Shooting()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > _nextFire) // A cooldown system
+        {
+            FireLaser();
+        }
+    }
+
+    private void FireLaser()
+    {
+        _nextFire = Time.time + _fireRate;
+        Vector3 positionOffset = new Vector3(transform.position.x, transform.position.y + 0.8f, transform.position.z);
+        Instantiate(_laserPrefab, positionOffset, Quaternion.identity);
+    }
+}
 
 
 
 
 
-}// Original Y Axis clamping (using if statements)
-        //if (transform.position.y >= _yCMaxVal)
-        //{
-        //    transform.position = new Vector3(transform.position.x, _yCMaxVal, transform.position.z);
-        //}
-        //else if (transform.position.y <= _yCMinVal)
-        //{
-        //    transform.position = new Vector3(transform.position.x, _yCMinVal, transform.position.z);
-        //}
+
+
+
+// Notes
+
+// Vector3.right is the equivalent of new Vector3(1,0,0).
+// Which means moving us 1 Unity unit (a meter) per frame (so roughly 60 meters per second)
+// Incorporate real time to get this to happen per second instead
+//transform.Translate(Vector3.right * _multiplier * Time.deltaTime);
+
+
+
+// Original Y Axis clamping (using if statements)
+//if (transform.position.y >= _yCMaxVal)
+//{
+//    transform.position = new Vector3(transform.position.x, _yCMaxVal, transform.position.z);
+//}
+//else if (transform.position.y <= _yCMinVal)
+//{
+//    transform.position = new Vector3(transform.position.x, _yCMinVal, transform.position.z);
+//}
