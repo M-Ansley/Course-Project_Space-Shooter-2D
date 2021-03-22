7fe41ae758f73e941242eaab5c081f9aecbb5b1c
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-03-22 19:07:20 +0000

    SpawnManager Initial Setup

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+        
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(_enemyPrefab);
+
+            yield return new WaitForSecondsRealtime(5f);
+        }
+    }
+}
