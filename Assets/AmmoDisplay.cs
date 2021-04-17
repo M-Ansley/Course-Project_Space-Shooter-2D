dfa70a161fbc024cdbcbbe1d9a514eb73840d14e
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-04-17 16:44:32 +0100

    Modern UI Pack Imported
    Have also made a start with the displays for ammo and the thrusters

---
+++ Assets/AmmoDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _gridGameObject = null;
+
+    [SerializeField]
+    private GameObject _ammoPrefab = null;
+
+    [SerializeField]
+    private List<GameObject> _ammoUnits = new List<GameObject>();
+
+    [SerializeField]
+    private int _maxAmmo = 15;
+
+    private void Start()
+    {
+        for (int i = 0; i < _maxAmmo; i++)
+        {
+            GameObject ammoUnit = Instantiate(_ammoPrefab, _gridGameObject.transform);
+            _ammoUnits.Add(ammoUnit);
+        }
+    }
+
+    public void DisplayAmmo(int ammoRemaining)
+    {
+
+    }
+
+
+}
